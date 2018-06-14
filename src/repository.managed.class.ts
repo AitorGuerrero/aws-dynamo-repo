acb2a91200cb451d5b51c656ca0e701a038a4169
Aitor Guerrero <aitor.guerrero@worldcoo.com>
2018-06-14 17:42:45 +0200

    managed repository extends cached repository

--- src/repository.managed.class.ts
+++ src/repository.managed.class.ts
@@ -1,28 +1,30 @@
 import {DynamoDB} from "aws-sdk";
 import DynamoEntityManager, {IEntityManagerTableConfig} from "./entity-manager.class";
 import generatorToArray from "./generator-to-array";
-import {DynamoDBRepository, IGenerator, ISearchInput} from "./repository.class";
+import {RepositoryCached} from "./repository.cached.class";
+import {IGenerator, ISearchInput} from "./repository.class";
 
-export class RepositoryManaged<Entity> {
+export class RepositoryManaged<Entity> extends RepositoryCached<Entity> {
 
 	constructor(
 		private entityName: string,
-		private tableConfig: IEntityManagerTableConfig<Entity>,
-		private repository: DynamoDBRepository<Entity>,
+		tableConfig: IEntityManagerTableConfig<Entity>,
+		documentClient: DynamoDB.DocumentClient,
 		private entityManager: DynamoEntityManager,
 	) {
+		super(documentClient, tableConfig);
 		this.entityManager.addTableConfig(entityName, tableConfig);
 	}
 
 	public async get(key: DynamoDB.DocumentClient.Key) {
-		const entity = await this.repository.get(key);
+		const entity = await super.get(key);
 		this.entityManager.track(this.entityName, entity);
 
 		return entity;
 	}
 
 	public async getList(keys: DynamoDB.DocumentClient.Key[]) {
-		const list = await this.repository.getList(keys);
+		const list = await super.getList(keys);
 		for (const entity of list.values()) {
 			this.entityManager.track(this.entityName, entity);
 		}
@@ -31,7 +33,7 @@ export class RepositoryManaged<Entity> {
 	}
 
 	public search(input: ISearchInput) {
-		const generator = this.repository.search(input);
+		const generator = super.search(input);
 		const managedGenerator = (async () => {
 			const entity = await generator();
 			this.entityManager.track(this.entityName, entity);
@@ -41,10 +43,6 @@ export class RepositoryManaged<Entity> {
 		managedGenerator.toArray = generatorToArray;
 
 		return managedGenerator;
-	}
-
-	public async count(input: ISearchInput) {
-		return this.repository.count(input);
 	}
 
 	public async persist(e: Entity) {
