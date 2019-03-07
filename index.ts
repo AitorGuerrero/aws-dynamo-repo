3ad68e989a2dbb9122d470f346ddf154331f1a16
Aitor Guerrero <aguerrerolsasarte@gmail.com>
2019-03-07 11:09:23 +0100

    added repository table config to index

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import RepositoryCached from "./src/cached/repository.class";
 import RepositoryManaged from "./src/managed/repository.class";
+import IRepositoryTableConfig, * as repositoryTableConfig from "./src/repository-table-config.interface";
 import DynamoDBRepository from "./src/repository.class";
 
 export {
 	DynamoDBRepository,
+	IRepositoryTableConfig,
+	repositoryTableConfig,
 	RepositoryCached,
 	RepositoryManaged,
 };
