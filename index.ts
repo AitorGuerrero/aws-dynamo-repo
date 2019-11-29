312aaffe90b018fc9d35f42dcf8790b56c2f17ed
Aitor Guerrero <aitor.guerrero@worldcoo.com>
2019-11-29 15:07:23 +0100

    added repositoryTableConfig to index

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import DynamoCachedRepository from "./src/cached/repository.class";
 import DynamoManagedRepository from "./src/managed/repository.class";
-import IRepositoryTableConfig from "./src/repository-table-config.interface";
+import IRepositoryTableConfig, * as repositoryTableConfig from "./src/repository-table-config.interface";
 import DynamoRepository from "./src/repository.class";
 
 export {
 	DynamoRepository,
 	IRepositoryTableConfig,
+	repositoryTableConfig,
 	DynamoCachedRepository,
 	DynamoManagedRepository,
 };
