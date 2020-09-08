bcd191918c2bf49fac6823ad6bedae629826d2d5
Aitor Guerrero <aitor.guerrero@worldcoo.com>
2020-09-08 12:42:43 +0200

    upgraded typescript

--- src/fake-document-client.class.ts
+++ src/fake-document-client.class.ts
@@ -144,7 +144,7 @@ export default class FakeDocumentClient {
 		await this.awaitFlush();
 		this.guardShouldFail(cb);
 		const item = await this.getByKey(input.TableName, input.Key);
-		const updates: Array<{k: string, v: any}> = /UPDATE/.test(input.UpdateExpression) ?
+		const updates: {k: string, v: any}[] = /UPDATE/.test(input.UpdateExpression) ?
 			/UPDATE ([^,]*)/.exec(input.UpdateExpression)[1]
 				.split(" AND ").map((s) => s.replace(" ", "").split("="))
 				.map((s) => ({k: s[0], v: s[1]})) :
